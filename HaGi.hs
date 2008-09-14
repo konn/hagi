c4c6d7818ac9f2b842b5335129358792ad277c15
Hiromi Ishii <mr_konn@jcom.home.ne.jp>
2008-09-15 01:28:12 +0900

    changed the way of prccessing primitives

--- HaGi.hs
+++ HaGi.hs
@@ -9,17 +9,21 @@ import Control.Monad.State
 
 data Inst = App Int Int | Abs Int [Inst] deriving Show
 
-type Code = [Inst]
-data Func = F (Code, Environment) | Succ | Out | Char Char | In  deriving Show
+data Code = C [Inst] | Succ | Out | Char Char | In  deriving Show
+data Func = F (Code, Environment) deriving Show
 type Environment = [Func]
-type DStack = [Func]
+type Dump = [Func]
 
 
 data GrassState = GS {
-  code :: Code, env :: Environment, dstack :: DStack
+  code :: Code, env :: Environment, dump :: Dump
 }
 
-mkGS c = GS c [Out,Succ,Char 'w',In] [F ([App 1 1],[]), F([],[])]
+mkGS c = GS{
+  code = C c,
+  env  = [F (Out, []),F (Succ, []),F (Char 'w', []),F (In, [])],
+  dump = [F(C [App 1 1], []), F(C [], [])]
+}
 
 instance Show GrassState where
   show (GS a b c) = show (a, b, c)
@@ -50,29 +54,35 @@ prog =try(do
           eof
           return (head:rest)
 
-trans (GS (App m n:c) e d) = do let fn = e !! (n-1)
-                                    fm = e !! (m-1)
-                                case fm of
-                                  (F (cm, em)) -> return $ GS cm (fn:em) (F (c, e):d)
-                                  _            -> do  f <- (mproc fm fn)
-                                                      return $ GS c (f:e) (F (c, e):d)
-  where mproc Succ (Char k)  = return $ Char (chr $ (ord k+1)`mod`256)
-        mproc Succ _         = error "Not char"
-        mproc Out c@(Char k) = U.putStr [k] >> (return$Char k)
-        mproc Out _         = error "Not char"
-        mproc In  c@(Char k) = (getChar >>= (return . Char))-- `catch` (return c)
-        mproc In _         = error "Not char"
-        mproc (Char k) (Char v) = return$Char k
+trans (GS (C(App m n:c)) e d) = do  let fn = e !! (n-1)
+                                        (F (cm, em)) = e !! (m-1)
+                                    return $ GS cm (fn:em) ((F(C c, e)):d)
 
-trans (GS (Abs 1 c':c) e d) = return $ GS c (F (c',e):e) d
-trans (GS (Abs n c':c) e d) = return $ GS c (F ([Abs(n-1)c'],e):e) d
-trans (GS [] (f:_) (F (c', e'):d)) = return $ GS c' (f:e') d
+trans (GS (C(Abs 1 c':c)) e d) = return $ GS (C c) ((F (C c',e)):e) d
+trans (GS (C(Abs n c':c)) e d) = return $ GS (C c) ((F (C [Abs(n-1)c'],e)):e) d
+trans (GS (C[]) (f:_) ((F (c', e')):d)) = return $ GS c' (f:e') d
 
-main = do (x:_) <- getArgs
-          src <- U.readFile x
-          let gs = mkGS $ parse prog src
+trans (GS prim e@(F(arg,_):_) d) = do rs <- mproc prim
+                                      return $ GS (C []) (F(rs, e):e) d
+  where mproc Succ = charP (\k -> return $ Char (chr $ (ord k+1)`mod`256) )
+        mproc Out = charP (\k -> U.putStr [k] >> (return arg))
+        mproc In  = (getChar >>= (return . Char) )`catch` (\_->return arg)
+        mproc (Char k) = charP(\k -> return$Char k)
+        
+        charP expr = case arg of
+                      (Char k) -> expr k
+                      _        -> error "argument must be character"
+
+main = do args <- getArgs
+          s <- case args of
+                  (x:_) -> U.readFile x
+                  _     -> U.getContents
+          let src = ppp s
+              gs = mkGS $ parse prog src
               go gs = do  gs <- (trans gs)
                           case gs of
-                            (GS [] [x] []) -> return ()
+                            (GS (C[]) [x] []) -> return ()
                             _ -> go gs
           go gs
+
+ppp = concatMap (\c->if c`elem`"wWvｗＷｖ"then [c]else[])
